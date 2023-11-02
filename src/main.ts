6a01976081f092e171b872da6a31afdc02e42630
martin-pabst <asv.pabst@gmail.com>
2023-11-02 07:20:24 +0100

    rename

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ export class Main {
   codeOutputEditor: monaco.editor.IStandaloneCodeEditor;
 
   tabManager: TabManager;
-
+ 
   tokenDiv: HTMLDivElement;
   astDiv: HTMLDivElement;
   codeOutputDiv: HTMLDivElement;
