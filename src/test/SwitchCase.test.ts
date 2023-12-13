b6fb080598e9cceae18f9cf4676c7185d3f969a7
tobido <tobias.dorsch@googlemail.com>
2023-12-13 11:06:26 +0100

    Add new test.

--- src/test/SwitchCase.test.ts
+++ src/test/SwitchCase.test.ts
@@ -93,3 +93,55 @@ test('test switch case with int', () => {
     expect(printSpy).toHaveBeenCalledWith("E", true, undefined);
 
 });
+
+test('test switch case with constant', () => {
+  let file = new File();
+  let code =`
+  public class A {
+    public static final String a = "ABC";
+    public static final int x = 5;
+  }
+  String b = "DEF";
+  int y = 4;
+  switch(b) {
+      case A.a: println("A"); break;
+      
+      default: switch(y) {
+        case 4:
+        case 5: println("B"); break;
+        default: println("C"); break;
+      }
+  }
+  println("D");
+  `;
+  
+  file.setText(code);
+
+  
+  // Mock TestPrintManager
+  // Method calls have no effect
+  // but can be spied on
+  vi.mock('../testgui/TestPrintManager', () => {
+    const TestPrintManager = vi.fn(() => ({
+      clear: vi.fn(),
+      print: vi.fn()
+    }))
+    return { TestPrintManager }
+  })
+  
+  
+  let tpm = new TestPrintManager();
+  let printSpy = vi.spyOn(tpm, 'print');  
+  
+  let compiler = new JavaCompiler();
+  let executable = compiler.compile(file);
+
+  let interpreter = new Interpreter(tpm);
+  interpreter.setExecutable(executable);
+
+  interpreter.runMainProgramSynchronously();
+
+  expect(printSpy).toHaveBeenCalledWith("B", true, undefined);
+  expect(printSpy).toHaveBeenCalledWith("D", true, undefined);
+
+});
