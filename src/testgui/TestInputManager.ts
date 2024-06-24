2b76d91c51579552245f91961cbe717690e01654
martin-pabst <asv.pabst@gmail.com>
2024-06-24 20:56:14 +0200

    ...

--- src/testgui/TestInputManager.ts
+++ src/testgui/TestInputManager.ts
@@ -1,1 +1,8 @@
-export class TestInputManager implements 
+import { IInputManager, InputManagerCallback } from "../compiler/common/interpreter/IInputManager.ts";
+
+export class TestInputManager implements IInputManager{
+    readInput(question: string, defaultValue: string, callback: InputManagerCallback): void {
+        
+    }
+
+}
